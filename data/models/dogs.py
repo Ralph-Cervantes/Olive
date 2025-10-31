6e9ad21f11f00872f5a5dfb2acdaf16d3ef08b83
ralph <rafael.g.cervantes@gmail.com>
2025-10-30 20:20:32 -0700

    Small changes

--- data/models/dogs.py
+++ data/models/dogs.py
@@ -6,5 +6,5 @@ class Dog(Base):
     __tablename__ = "dogs"
 
     id = Column(Integer, primary_key=True, index=True)
-    breed = Column(String, index=True)
+    breed = Column(String, index=True, unique=True)
     image = Column(String, nullable=True, default=None)
